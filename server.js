8f459cf00bf9e6ce296c028ebf810ba3596a5b2b
Mjohnwright <mjohnwright@hotmail.com>
2018-09-23 13:49:32 -0400

    fixed db connection

--- server.js
+++ server.js
@@ -6,7 +6,6 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var axios = require("axios");
 
 // Sets up the Express App
 // =============================================================
