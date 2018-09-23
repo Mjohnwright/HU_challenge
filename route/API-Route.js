d462942ed3811ba02999f46e17f531c2d57d91b6
Mjohnwright <mjohnwright@hotmail.com>
2018-09-23 14:08:27 -0400

    add color

--- route/API-Route.js
+++ route/API-Route.js
@@ -3,7 +3,7 @@ var db = require("../model");
 
 module.exports = function(app) {
 
-  // POST route for saving a new todo. You can create an email lead using the data on req.body
+  // POST route for saving a new email. You can create an email lead using the data on req.body
   app.post("/api/email", function(req, res) {
     console.log("posted");
   });
